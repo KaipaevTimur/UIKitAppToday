c846d7e9c1545552190221eb0d4d7f3a6719ab3e
Kaipaev Timur <kaipaevtimur55@gmail.com>
2022-06-30 14:47:22 +0200

    added new file ReminderViewController+Section

---
+++ Today/DetailViewController/ReminderViewController+Section.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderViewController+Section.swift
+//  Today
+//
+//  Created by Admin on 30.06.2022.
+//
+
+import UIKit
+
+extension ReminderViewController {
+    enum Section: Int, Hashable {
+        case view
+        case title
+        case date
+        case notes
+    }
+}
