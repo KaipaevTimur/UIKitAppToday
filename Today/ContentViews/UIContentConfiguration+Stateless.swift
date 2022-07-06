b48b85b19ec75d107c2c0afdc94ef41a5047c9d9
Kaipaev Timur <kaipaevtimur55@gmail.com>
2022-07-06 12:08:23 +0200

    Created new file TextFieldContentView and UIContentConfiguration+Stateless Added new class TextFieldContentView

---
+++ Today/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,15 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Today
+//
+//  Created by Admin on 06.07.2022.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
